b65ce07b49cac8f29d3bf0f742276c39d12effd7
Raghad-syd <rsd878@uowmail.edu.au>
2024-11-20 17:37:47 +0400

    Update Code.c
    Initializing the robot, sensors and motors

---
+++ Code.c
@@ -0,0 +1,43 @@
+#include <webots/robot.h>
+#include <webots/light_sensor.h>
+#include <webots/distance_sensor.h>
+#include <webots/motor.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define TIME_STEP 64
+#define MAX_SPEED 6.28
+#define MAX_READINGS 10 // Max number of dead ends to record
+#define THRESHOLD 100
+
+int main(int argc, char **argv) {
+  wb_robot_init();
+
+  // Initialize motors
+  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
+  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_velocity(left_motor, 0.0);
+  wb_motor_set_velocity(right_motor, 0.0);
+
+  // Initialize distance sensors
+  WbDeviceTag ps[8];
+  for (int i = 0; i < 8; i++) {
+    char sensor_name[4];
+    sprintf(sensor_name, "ps%d", i);
+    ps[i] = wb_robot_get_device(sensor_name);
+    wb_distance_sensor_enable(ps[i], TIME_STEP);
+  }
+
+  // Initialize light sensors
+  WbDeviceTag ls[8];
+  for (int i = 0; i < 8; i++) {
+    char sensor_name[4];
+    sprintf(sensor_name, "ls%d", i);
+    ls[i] = wb_robot_get_device(sensor_name);
+    wb_light_sensor_enable(ls[i], TIME_STEP);
+  }
+  wb_robot_cleanup();
+  return 0;
+}
